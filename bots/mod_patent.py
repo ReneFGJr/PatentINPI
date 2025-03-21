5054e149d549fb1ad76d5a9be0f91508a8679578
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-03-21 12:35:43 -0300

    Update mod_patent.py

--- bots/mod_patent.py
+++ bots/mod_patent.py
@@ -4,21 +4,23 @@ import re
 
 
 def clearNPR(nr='BR 20 2017 015440-3 Y1'):
-    t = {'A1','A2','A3','A4','A5','A6','A7','A8',
-         'B1', 'B2','B8',
-         'C1','C8',
-         'E1','E2','E3','E4','E5','E6','E7','E8',
-         'F1','F2','F3','F4','F5','F6','F7','F8',
-         'G1','G2','G3','G4','G5','G6','G7','G8',
-         'U1','U2','U3','U4','U5','U6','U7','U8',
-         'Y1','Y2','Y3','Y4','Y5','Y6','Y7','Y8',
-         'Z1','Z2','Z3','Z4','Z5','Z6','Z7','Z8'}
+    t = {
+        'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8',
+        'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8',
+        'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8',
+        'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8',
+        'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8',
+        'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8',
+        'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8',
+        'U1', 'U2', 'U3', 'U4', 'U5', 'U6', 'U7', 'U8',
+        'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6', 'Y7', 'Y8',
+        'Z1', 'Z2', 'Z3', 'Z4', 'Z5', 'Z6', 'Z7', 'Z8'
+    }
 
     # Divide a string em partes e verifica se o sufixo final está na lista proibida
     partes = nr.split()
     if partes[-1] in t:
         nr = nr.replace(partes[-1], '')
-
 
     # Remove todos os caracteres não numéricos
     nr = nr.replace('\n', '')
@@ -34,6 +36,7 @@ def clearNPR(nr='BR 20 2017 015440-3 Y1'):
 
     return nr
 
+
 def extrair_numeros_patentes(conteudo):
     """
     Extrai todos os números de patentes do conteúdo do arquivo,
@@ -48,11 +51,11 @@ def verificar_e_inserir_patente(numero_patente):
     Verifica se a patente já existe no banco. Se não existir, insere.
     """
     numero_patente = numero_patente.strip()
-    qr = "SELECT COUNT(*) FROM rpi_patent_nr WHERE p_nr = '"+numero_patente+"'"
+    qr = "SELECT COUNT(*) FROM rpi_patent_nr WHERE p_nr = '" + numero_patente + "'"
     rows = database.query(qr)
     row = rows[0]
     if row[0] == 0:
-        qi = "INSERT INTO rpi_patent_nr (p_nr) VALUES ('"+numero_patente+"')"
+        qi = "INSERT INTO rpi_patent_nr (p_nr) VALUES ('" + numero_patente + "')"
         database.insert(qi)
         print(f"Patente {numero_patente} adicionada ao banco.")
 
